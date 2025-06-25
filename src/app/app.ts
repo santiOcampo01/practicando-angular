4b4ac97f08630863862093fe07265c399e9bad3f
santiOcampo01 <santimejiaok@gmail.com>
2025-06-25 14:28:56 -0500

    first spets

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TablaAlumnos} from './tabla-alumnos/tabla-alumnos';
+import { TablaCursos } from './tabla-cursos/tabla-cursos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TablaAlumnos, TablaCursos],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected title = 'listaCursos';
+  cursos: boolean = true;
+  handleChange(){
+    this.cursos = !this.cursos;
+  }
 }
